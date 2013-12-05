4e19cdd97932c246a8bc388956e99433d111bde0
kokushozero <lfewings@internode.on.net>
2013-12-05 14:17:08 +1030

    Added user management functionality. Polls connected users to determine if any clients have disconnected.

--- NetworkingChatProgram/src/ServerSide/Server.java
+++ NetworkingChatProgram/src/ServerSide/Server.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 /*
@@ -15,12 +16,12 @@ public class Server implements Runnable{
 
 	private int port;
 	private DatagramSocket socket, sendSocket;
-	private Thread run, users, post, listen;
+	private Thread run, users, post, listen, check;
 	private boolean ServerRunning;
 	private ArrayList<User> userList;
 	private ObjectInputStream inStream;
 	private ByteArrayInputStream byteInStream;
-
+	private DatagramSocket udpPing;
 
 	public Server(int port){
 		this.port = port;
@@ -36,9 +37,58 @@ public class Server implements Runnable{
 		run.start();
 	}
 
+	/**
+	 * Polls the userList, sends UDP packets to all clients at a regular interval to determine if any clients have since disconnected.
+	 */
+	private void checkWhoIsStillAlive(){
+		check = new Thread("Check"){
+			public void run(){
+				
+				try {
+					udpPing = new DatagramSocket();									
+				} catch (Exception e1) {
+					System.out.println(e1.getMessage());
+					e1.printStackTrace();
+				}
+				
+				while (ServerRunning){
 
-	/*
-	 * Listens for incoming logins from Clients and records them in an ArrayList.
+					// Delay the thread by 2 seconds per cycle to ameliorate network congestion.
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e1) {
+						System.out.println(e1.getMessage());
+						e1.printStackTrace();
+					}
+					
+					for (int i = 0; i < userList.size();i++){
+						byte[] payload = new byte[1024];
+						DatagramPacket packet = new DatagramPacket(payload, payload.length, userList.get(i).getIp(), userList.get(i).getPort()+1); // Creates a packet with user port num + 1.
+						try {
+							udpPing.send(packet);
+							payload = new byte[1024];
+							packet = new DatagramPacket(payload, payload.length);
+							udpPing.setSoTimeout(3000); // Will throw a SocketTimeoutException if the socket does not receive a packet within 3 seconds.
+							udpPing.receive(packet);
+						} catch (SocketTimeoutException e) {
+							System.out.println(e.getMessage());
+							System.out.println(userList.get(i).getUserName()+" has disconnected");
+							userList.remove(i);
+						} catch (Exception e1){
+							System.out.println(e1.getMessage());
+							e1.printStackTrace();
+						}
+					}
+					
+				}
+			}
+		};
+		check.start();
+
+	}
+
+	/**
+	 * Listens for incoming logins from Clients and records them and the Clients parameters in an ArrayList.
 	 */
 	private void manageUsers(){
 		users = new Thread("Users"){
@@ -54,15 +104,27 @@ public class Server implements Runnable{
 						byteInStream = new ByteArrayInputStream(payload);
 						inStream = new ObjectInputStream(byteInStream);
 						User user = (User) inStream.readObject();
-						byteInStream.close();
-						inStream.close();
-						if (!userList.contains(user)){
+						
+						// Checks if the user's IP address has already been registered as a client. If it is not currently active it will be added as
+						// a valid client.
+						boolean inList = false;
+						for (int i = 0; i < userList.size(); i++){
+							if (user.getIp().equals(userList.get(i).getIp())){
+								inList = true;
+							}
+						}
+						if (inList == false){
 							userList.add(user);
 						}
+						
+						
+						byteInStream.close();
+						inStream.close();
 						System.out.println("**CONNECTED USERS**");
 						for (int i = 0; i < userList.size(); i++){
-							System.out.println(userList.get(i).getUserName()+" on "+ userList.get(i).getIp().getHostAddress()+"\n**********************");
+							System.out.println(userList.get(i).getUserName()+" on "+ userList.get(i).getIp().getHostAddress());
 						}
+						System.out.println("\n**********************");
 					}
 					socket.close();
 				} catch (Exception e) {
@@ -85,6 +147,7 @@ public class Server implements Runnable{
 
 		for (int i = 0; i < userList.size(); i++){
 			byte[] payload = message.getBytes();
+			System.out.println("Sending message to "+ userList.get(i).getIp().getHostAddress()+ " on "+userList.get(i).getPort());
 			DatagramPacket packet = new DatagramPacket(payload, payload.length, userList.get(i).getIp(), userList.get(i).getPort());
 			try {
 				sendSocket.send(packet);
@@ -114,7 +177,7 @@ public class Server implements Runnable{
 						e.printStackTrace();
 					}
 					String message = new String(packet.getAddress()+" on port "+packet.getPort()+": "+ new String(packet.getData()));
-					System.out.println(message);
+					System.out.println(message+"\n");
 					sendToClients(message);
 				}
 			}
@@ -129,6 +192,7 @@ public class Server implements Runnable{
 		System.out.println("Server running on port "+this.port);
 		manageUsers();
 		listen();
+		checkWhoIsStillAlive();
 	}
 
 
